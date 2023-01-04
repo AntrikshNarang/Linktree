bb5e2f2f87d1f0e305713e2a13942c57f2c87e06
AntrikshNarang <itsantriksh@gmail.com>
2023-01-04 23:40:29 +0530

    Added Hover Effect Using JS

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 for(var i=0;i<document.querySelectorAll(".link").length;i++){
     document.querySelectorAll(".link")[i].addEventListener("mouseover",function(){
-        
+        this.classList.add("link-hoverjs");
+    })
+}
+for(var i=0;i<document.querySelectorAll(".link").length;i++){
+    document.querySelectorAll(".link")[i].addEventListener("mouseout",function(){
+        this.classList.remove("link-hoverjs");
     })
 }
